4041ce246bf21bf1dd48b7c22f6ea2e62757c1bb
Trofin-G <gabriel.trofin@landofweb.ro>
2024-12-18 16:22:57 +0200

    upp files

---
+++ axios-temaa/src/components/CardItem.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="card-item demo-panel gray">
+        <div class="panel m-20">
+            <div class="p-20">
+                <p><b>Nume:</b> {{ item.name }}</p>
+                <p><b>Username:</b> {{ item.username }}</p>
+                <p><b>Email:</b> {{ item.email }}</p>
+                <p><b>Strada:</b> {{ item.address.street }}</p>
+                <p><b>Oras:</b> {{ item.address.city }}</p>
+                <p><b>Cod postal:</b> {{ item.address.zipcode }}</p>
+                <p><b>Telefon:</b> {{ item.phone }}</p>
+                <p><b>Site web:</b> {{ item.website }}</p>
+                <p><b>Nume companie:</b> {{ item.company.name }}</p>
+                <p><b>CatchPhrase companie:</b> {{ item.company.catchPhrase }}</p>
+                <p><b>Bs companie: </b> {{ item.company.bs }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        item: Object
+    },
+
+}
+</script>
+
+<style lang="scss">
+.card-item {
+    background-color: lightgray;
+    margin: 15px 0;
+
+    p {
+        &:first-of-type {
+            color: blue;
+        }
+    }
+
+    &:nth-child(2n) {
+        p {
+            &:first-of-type {
+                color: green;
+            }
+        }
+    }
+}
+</style>
---
+++ axios-temaa/src/components/CardsList.vue
@@ -0,0 +1,50 @@
+<template>
+    <div v-if="showFirstSix.length !== 0" class="cards-wrapper">
+        <CardItem v-for="item in showFirstSix" :key="item.id" :item="item" />
+        <!-- <button :class="{ 'hidden': listItems.length === showFirstSix.length }" @click="loadMore">Incarca inca 4 elemente</button> -->
+        <b-button :class="{ 'hidden': listItems.length === showFirstSix.length }" @click="loadMore" block variant="primary">Incarca inca 4 elemente</b-button>
+    </div>
+    <div v-else class="cards-wrapper">
+        <p>Nu exista nici un card.</p>
+    </div>
+</template>
+
+<script>
+import CardItem from '@/components/CardItem.vue'
+
+
+
+export default {
+    props: {
+        listItems: Array
+    },
+
+    components: {
+        CardItem
+    },
+
+    data() {
+        return {
+            length: 4,
+        }
+    },
+
+    methods: {
+        loadMore() {
+            if (this.length > this.showFirstSix.length) return;
+            this.length = this.length + 4;
+        },
+    },
+
+
+    computed: {
+        showFirstSix: function () {
+            return this.listItems.slice(0, this.length)
+        },
+    },
+
+}
+</script>
+
+<style lang="scss">
+</style>
